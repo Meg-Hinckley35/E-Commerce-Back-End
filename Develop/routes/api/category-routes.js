49f24670d5cd3a6e44fed12cfecaaa890924aed7
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-05 11:57:10 -0400

    added post request to add a new category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -56,6 +56,15 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  //by inputting the new category name
+  Category.create({
+    category_name: req.body.category_name,
+  })
+  .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
