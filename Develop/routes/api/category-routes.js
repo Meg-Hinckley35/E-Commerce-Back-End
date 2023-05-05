92ff5c3657ecdb6e938ff964deb6fd5688228a85
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-05 12:02:03 -0400

    setting up the put request to update an existing category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -69,6 +69,33 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  //use a post request to update an existing category
+  //sends an error message if category does not exist
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  .then((dbCategoryData) => {
+    if (!dbCategoryData) {
+      res
+        .status(404)
+        .json({
+          message: "The category you are trying to update does not exist!",
+        });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
