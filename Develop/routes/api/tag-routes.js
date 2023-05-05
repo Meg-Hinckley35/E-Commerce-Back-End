ee0e60f26b9b911ba58d1b15ceefec842e804bf2
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-05 12:21:02 -0400

    set get rquest to find just one tagand console log error if not found

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -29,7 +29,34 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // include its associated Product data
+  // console log error and send message if tag does not exist
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include:
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'The tag you are trying to find does not exist!' });
+        return;
+      }
+      res.json(dbTagData);
+    })    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
